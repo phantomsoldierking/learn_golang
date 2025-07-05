bcdead257ef2ec7a02855af8953ef507d0985028
phantomsoldierking <patilyogeshgouda@gmail.com>
2025-07-05 23:03:13 +0530

    :feat: basic CRUD

--- CRUD_api/main.go
+++ CRUD_api/main.go
@@ -27,6 +27,11 @@ var movies []Movie
 
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	parms := mux.Vars(r)
 	for _, item := range movies {
 		if item.ID == parms["id"] {
@@ -34,4 +39,54 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request( {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1]...)  // spread opp like in js??
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func main() {
+	r := mux.BewRouter()
+	
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/moives/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/moives", createMovie).Methods("POST")
+	r.HandleFunc("/moives/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/moives/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
