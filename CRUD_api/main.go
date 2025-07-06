11544ca964b23cca1cddb0c4b6a642963ae71648
phantomsoldierking <patilyogeshgouda@gmail.com>
2025-07-06 11:53:03 +0530

    :feat: completed CRUD

--- CRUD_api/main.go
+++ CRUD_api/main.go
@@ -41,7 +41,7 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func createMovie(w http.ResponseWriter, r *http.Request( {
+func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
@@ -55,7 +55,7 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for index, item := range movies {
 		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1]...)  // spread opp like in js??
+			movies = append(movies[:index], movies[index+1:]...)  // spread opp like in js??
 			var movie Movie
 			_ = json.NewDecoder(r.Body).Decode(&movie)
 			movie.ID = params["id"]
@@ -71,7 +71,7 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for index, item := range movies {
 		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1]...)
+			movies = append(movies[:index], movies[index+1:]...)
 			break
 		}
 	}
@@ -79,7 +79,7 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := mux.BewRouter()
+	r := mux.NewRouter()
 	
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/moives/{id}", getMovie).Methods("GET")
